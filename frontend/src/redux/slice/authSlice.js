3e532b4f6b9b21907ea9b9cb4239fc9a571df077
sdunga1 <sdunga1@asu.edu>
2025-04-13 20:30:38 -0700

    Cached the user info in local storge

--- frontend/src/redux/slice/authSlice.js
+++ frontend/src/redux/slice/authSlice.js
@@ -2,26 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 
 //! initial state
 const authSlice = createSlice({
-    name: 'auth',
-    initialState: {
-        user:null,
+  name: "auth",
+  initialState: {
+    user: JSON.parse(localStorage.getItem("userInfo")) || null,
+  },
+
+  //Reducers
+  reducers: {
+    loginAction: (state, action) => {
+      state.user = action.payload;
     },
 
-    //Reducers
-    reducers: {
-        loginAction: (state, action) => {
-            state.user = action.payload;
-        },
-
-        //Logout action
-        logoutAction: (state) => {
-            state.user = null;
-        }
-    }
-})
+    //Logout action
+    logoutAction: (state) => {
+      state.user = null;
+    },
+  },
+});
 
 //! Generate actions
-export const {loginAction, logoutAction} = authSlice.actions;
+export const { loginAction, logoutAction } = authSlice.actions;
 
 //! Generate the reducers
 const authReducer = authSlice.reducer;
