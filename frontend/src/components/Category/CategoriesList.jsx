43d443e0db9f79a11ac02f0bfbbcbc6f00f2460f
sdunga1 <sdunga1@asu.edu>
2025-04-29 21:40:58 -0700

    Added the scrollable and bug fix categories names

--- frontend/src/components/Category/CategoriesList.jsx
+++ frontend/src/components/Category/CategoriesList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { FaTrash, FaEdit } from "react-icons/fa";
+import { FaTrash, FaEdit, FaChevronDown } from "react-icons/fa";
 import { useQuery } from "@tanstack/react-query";
 import { Link, useLocation } from "react-router-dom";
 import { listCategoriesAPI } from "../../services/category/categoryServices";
@@ -9,7 +9,12 @@ const CategoriesList = () => {
   const { state } = useLocation();
   const updatedId = state?.updatedId;
 
-  const { data, isError, isLoading, error } = useQuery({
+  const {
+    data = [],
+    isError,
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: ["list-categories"],
     queryFn: listCategoriesAPI,
   });
@@ -20,6 +25,8 @@ const CategoriesList = () => {
     }
   }, [updatedId]);
 
+  const showScrollIcon = (data?.length ?? 0) > 7;
+
   return (
     <div className="max-w-md mx-auto my-10 bg-white p-6 rounded-lg shadow-2xl space-y-6 border border-gray-200">
       <h2 className="text-2xl font-semibold text-gray-800 mb-4">Categories</h2>
@@ -28,44 +35,52 @@ const CategoriesList = () => {
       {isError && (
         <AlertMessage type="error" message={error.response.data.message} />
       )}
-
-      <ul className="space-y-4">
-        {data?.map((category) => (
-          <li
-            key={category?._id}
-            className={`flex justify-between items-center bg-gray-50 p-3 rounded-md ${
-              category._id === updatedId ? "bump" : ""
-            }`}
-          >
-            <div>
-              <span className="text-gray-800">{category?.name}</span>
-              <span
-                className={`ml-2 px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-                  category.type === "income"
-                    ? "bg-green-100 text-green-800"
-                    : "bg-red-100 text-red-800"
-                }`}
-              >
-                {category?.type?.charAt(0).toUpperCase() +
-                  category?.type?.slice(1)}
-              </span>
-            </div>
-            <div className="flex space-x-3">
-              <Link to={`/update-category/${category._id}`}>
-                <button className="text-blue-500 hover:text-blue-700">
-                  <FaEdit />
+      <div className="relative">
+        <ul
+          className={`space-y-4 ${
+            showScrollIcon ? "max-h-80 overflow-y-auto pr-2" : ""
+          }`}
+        >
+          {data.map((category) => (
+            <li
+              key={category._id}
+              className={`flex justify-between items-center bg-gray-50 p-3 rounded-md ${
+                category._id === updatedId ? "bump" : ""
+              }`}
+            >
+              <div>
+                <span className="text-gray-800">{category.name}</span>
+                <span
+                  className={`ml-2 px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
+                    category.type === "income"
+                      ? "bg-green-100 text-green-800"
+                      : "bg-red-100 text-red-800"
+                  }`}
+                >
+                  {category.type.charAt(0).toUpperCase() +
+                    category.type.slice(1)}
+                </span>
+              </div>
+              <div className="flex space-x-3">
+                <Link to={`/update-category/${category._id}`}>
+                  <button className="text-blue-500 hover:text-blue-700">
+                    <FaEdit />
+                  </button>
+                </Link>
+                <button className="text-red-500 hover:text-red-700">
+                  <FaTrash />
                 </button>
-              </Link>
-              <button
-                // onClick={() => handleDelete(category?._id)}
-                className="text-red-500 hover:text-red-700"
-              >
-                <FaTrash />
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        {showScrollIcon && (
+          <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 animate-bounce">
+            <FaChevronDown className="text-gray-400" />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
